e71bc6c247e8874a0b7ca1f9bee9818a1ca31c05
24LP8639 24LP8639 <24LP8639@G305-employeeiMac.local>
2025-01-09 18:38:41 +0800

    frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,148 @@
-'use client'
+"use client";
+import { ok } from "assert";
 import { useEffect, useState } from "react";
 
 type Movie = {
   id: number;
   name: string;
-}
+};
 export default function Home() {
-  const [movies, setMovies] = useState<Movie[]>([])
-async function getMovies(){
-  const res = await fetch('http://localhost:4000/movies')
-  const data = await res.json()
-  setMovies(data)
-}
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [newMovie, setNewMovie] = useState<Movie>();
+  const [name, setName] = useState<string>("");
+  const [id, setId] = useState<number>(0);
 
+  async function getMovies() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/movies`);
+    const data = await res.json();
+    setMovies(data);
+  }
+  async function addMovie() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/create`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+      }),
+    });
+    const data = await res.json();
+    setMovies(data);
+  }
+  async function movieDetails(id: number) {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/details/${id}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await res.json();
+    setNewMovie(data);
+  }
+  async function updateMovie(id: number) {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/update${id}`,
+      {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await res.json();
+  }
+  async function deleteMovie(id: number) {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/movies/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await res.json();
+    deleteMovie(data);
+    // if (res) {
+    //   getMovies();
+    //   console.log(res);
+    // } else {
+    //   alert("failed");
+    // }
+  }
   useEffect(() => {
-
-getMovies()
-  },[])
+    getMovies();
+    console.log(movies);
+  }, []);
   return (
     <div>
-      <button>
-        
-      {movies.map((movie) => (
-        <div key={movie.id}>{movie.name}</div>
-      ))}
-      </button>
-      
-    
+      <div className="w-[300px] border">
+        <div className="button-add">
+          <button
+            onClick={() => {
+              addMovie();
+            }}
+            className="border rounded w-[50px]"
+          >
+            Add
+          </button>
+          <input
+            onChange={(e) => {
+              setName(e.target.value);
+              console.log(name);
+            }}
+          />
+        </div>
+        <div className="button-find">
+          <button
+            onClick={() => {
+              movieDetails(id);
+            }}
+            className="border rounded w-[50px]"
+          >
+            find
+          </button>
+          <input
+            onChange={(e) => {
+              setId(Number(e.target.value));
+              console.log(typeof id);
+            }}
+          />
+          <div>{newMovie?.name}</div>
+        </div>
+
+        {movies.map((movie) => (
+          <div key={movie.id} className="flex justify-between">
+            <div>{movie.name}</div>
+            <div className="gap-5">
+              <button
+                className="border rounded"
+                onClick={() => {
+                  updateMovie(movie.id);
+                }}
+              >
+                Edit
+              </button>
+              <button
+                className="border rounded"
+                onClick={() => {
+                  deleteMovie(movie.id);
+                }}
+              >
+                delete
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
